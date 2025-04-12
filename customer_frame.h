fb2d68e99b6b27f64d24c32548ff7ca4b1db14c1
K24dtcn380 <phucnh893@gmail.com>
2025-04-12 13:44:01 +0700

    Update customer_frame.h

--- customer_frame.h
+++ customer_frame.h
@@ -10,3 +10,30 @@ class CustomerFrame : public wxFrame {
 public:
     CustomerFrame(const wxString& title, int userId);
     ~CustomerFrame();
+
+private:
+    int userId;
+    Customer* customer;
+    wxNotebook* notebook;
+    wxTextCtrl* outputText;
+    wxTextCtrl* oldPasswordInput;
+    wxTextCtrl* newPasswordInput;
+    wxTextCtrl* receiverUsernameInput;
+    wxTextCtrl* transferPointsInput;
+    wxTextCtrl* searchUsernameInput;
+    wxTextCtrl* nameInput;
+    wxTextCtrl* dobInput;
+    wxTextCtrl* addressInput;
+    wxTextCtrl* historyText;
+
+    void OnViewInfo(wxCommandEvent& event);
+    void OnChangePassword(wxCommandEvent& event);
+    void OnTransferPoints(wxCommandEvent& event);
+    void OnLogout(wxCommandEvent& event);
+    void OnClose(wxCloseEvent& event);
+    void OnSearchCustomer(wxCommandEvent& event);
+    void OnUpdateInfo(wxCommandEvent& event);
+    void OnViewHistory(wxCommandEvent& event);
+};
+
+#endif
